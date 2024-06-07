faf572b2e379f6be1dfd1da30d6957e8b79c757d
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-06-07 18:30:15 -0300

    feat: Add banks List

---
+++ src/views/pages/bancos/List/RowOptions.tsx
@@ -0,0 +1,86 @@
+import { useState, MouseEvent } from 'react'
+import { Button, IconButton, Menu, MenuItem, useMediaQuery, Box } from '@mui/material'
+import Icon from 'src/@core/components/icon'
+import { api } from 'src/services/api'
+import toast from 'react-hot-toast'
+
+interface BankStatusType {
+  [key: string]: string
+}
+
+const banckStatus: BankStatusType = {
+  ACTIVE: 'Inativar',
+  INACTIVE: 'Ativar'
+}
+
+interface RowOptionsProps {
+  id: string
+  status: string
+  refreshData: () => void
+}
+
+const RowOptions = ({ id, status, refreshData }: RowOptionsProps) => {
+  const matches = useMediaQuery('(min-width:600px)')
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const rowOptionsOpen = Boolean(anchorEl)
+
+  const handleRowOptionsClick = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleRowOptionsClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleActiveOrInactive = () => {
+    api
+      .put(`/banks/${id}`, { status: status === 'ACTIVE' ? 'INACTIVE' : 'ACTIVE' })
+      .then(() => refreshData())
+      .catch(() => toast.error('Erro ao alterar status do banco'))
+  }
+
+  return (
+    <>
+      {matches ? (
+        <Box
+          sx={{
+            display: 'flex',
+            gap: 2,
+            padding: '0 8 0 0 '
+          }}
+        >
+          <Button size='small' variant='outlined' color='primary' onClick={handleActiveOrInactive}>
+            {banckStatus[status]}
+          </Button>
+        </Box>
+      ) : (
+        <>
+          <IconButton size='small' onClick={handleRowOptionsClick}>
+            <Icon icon='tabler:dots-vertical' />
+          </IconButton>
+          <Menu
+            keepMounted
+            anchorEl={anchorEl}
+            open={rowOptionsOpen}
+            onClose={handleRowOptionsClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right'
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right'
+            }}
+            PaperProps={{ style: { minWidth: '8rem' } }}
+          >
+            <MenuItem onClick={handleActiveOrInactive}>{banckStatus[status]}</MenuItem>
+          </Menu>
+        </>
+      )}
+    </>
+  )
+}
+
+export default RowOptions
