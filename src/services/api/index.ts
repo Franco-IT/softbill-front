3bba77080e8f858af76a3ded9a800d30d80f4278
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-08-28 15:21:01 -0300

    feat: Add camel case to snack case conversion middleware.

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,10 +1,23 @@
 import axios from 'axios'
+import applyCaseMiddleware from 'axios-case-converter'
 
-export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    'x-api-key': process.env.NEXT_PUBLIC_API_KEY
+const options = {
+  caseMiddleware: {
+    requestInterceptor: (config: any) => {
+      // Disable query string transformation
+      return config
+    }
   }
-})
+}
+
+export const api = applyCaseMiddleware(
+  axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'x-api-key': process.env.NEXT_PUBLIC_API_KEY
+    }
+  }),
+  options
+)
