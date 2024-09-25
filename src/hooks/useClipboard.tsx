3402d907e2158075c8eaed43bf96943288c4bc2d
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-09-25 08:46:20 -0300

    fix: Resolves problem with copying first access URL.

--- src/hooks/useClipboard.tsx
+++ src/hooks/useClipboard.tsx
@@ -1,15 +1,39 @@
+// React Imports
 import { useCallback } from 'react'
+
+// External Libraries
 import toast from 'react-hot-toast'
 
+const fallbackCopyToClipboard = (text: string) => {
+  const textArea = document.createElement('textarea')
+  textArea.value = text
+  textArea.style.position = 'fixed'
+  textArea.style.opacity = '0'
+  document.body.appendChild(textArea)
+  textArea.select()
+  try {
+    document.execCommand('copy')
+    toast.success('Texto copiado com sucesso!')
+  } catch (err) {
+    toast.error('Erro ao copiar para a área de transferência.')
+  } finally {
+    document.body.removeChild(textArea)
+  }
+}
+
 const useClipboard = () => {
-  const copyToClipboard = useCallback((text: string, message: string) => {
+  const copyToClipboard = useCallback(async (text: string, message = 'Texto copiado com sucesso!') => {
+    if (!navigator.clipboard) {
+      fallbackCopyToClipboard(text)
+
+      return
+    }
+
     try {
-      if (message) {
-        toast.success(message)
-        navigator.clipboard.writeText(text)
-      }
+      await navigator.clipboard.writeText(text)
+      toast.success(message)
     } catch (error) {
-      toast.error('Erro ao copiar para a área de transferência, tente novamente')
+      toast.error('Erro ao copiar para a área de transferência. Tente novamente.')
     }
   }, [])
 
