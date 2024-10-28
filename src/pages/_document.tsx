3ac81640735445ee7d18fc4c8d1fa443992337ad
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-10-28 13:51:36 -0300

    feature: Adds Google tag manager.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,6 @@ import createEmotionServer from '@emotion/server/create-instance'
 
 // ** Utils Imports
 import { createEmotionCache } from 'src/@core/utils/create-emotion-cache'
-import Script from 'next/script'
 
 class CustomDocument extends Document {
   render() {
@@ -25,15 +24,15 @@ class CustomDocument extends Document {
           />
           <link rel='apple-touch-icon' sizes='180x180' href='/images/favicon.png' />
           <link rel='shortcut icon' href='/images/favicon.png' />
-          <Script id='google-tag-manager' strategy='lazyOnload'>
-            {`
-            function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KHKQ4GS5');
-        `}
-          </Script>
+            })(window,document,'script','dataLayer','GTM-KHKQ4GS5');`
+            }}
+          />
         </Head>
         <body>
           <Main />
